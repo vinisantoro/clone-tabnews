b7d2a0d9d429cc39b5a925b61168cb74f1745686
Vinicius Santoro <vini.santoro@gmail.com>
2024-02-16 20:00:06 +0000

    adicionado os scripts de banco

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,7 @@
 import database from "infra/database";
 
 async function status(request, responde) {
-  const result = await database.query("SELECT 1 + 1");
-  console.log(result.rows);
-  responde.status(200).json({ chave: "são acima da média" });
+  responde.status(200).json({});
 }
 
 export default status;
