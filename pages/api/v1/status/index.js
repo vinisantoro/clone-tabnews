3603544cb0158641b70bd81aefa2146cb10a4acd
Vinicius Santoro <vini.santoro@gmail.com>
2024-02-16 19:04:02 +0000

    arquivos de base

---
+++ pages/api/v1/status/index.js
@@ -0,0 +1,9 @@
+import database from "../../../../infra/database";
+
+async function status(request, responde) {
+  const result = await database.query("SELECT 1 + 1");
+  console.log(result.rows);
+  responde.status(200).json({ chave: "são acima da média" });
+}
+
+export default status;
